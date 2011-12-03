ebe2aa7d518bb48c2b0864422a3fed528c418f92
Vladimir Tsvang <vtsvang@gmail.com>
2011-12-03 13:27:34 +0100

    initial commit

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+rem use utf-8 encoding in console output:
+node build.js %*
---
+++ examples/test.tpl.js
@@ -0,0 +1,10 @@
+(function($, w, d, undefined) {
+	
+//<!include file="./foo.js"-->
+//<!include file="./bar.js"-->
+
+alert(foo());
+alert(bar());
+
+	
+})(jQuery, window, document);
